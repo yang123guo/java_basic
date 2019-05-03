6aa1922d700bce17f735e03da7f6a3dbd1f47339
yangguoqiang01 <wochange@163.com>
2019-05-04 00:23:49 +0800

    :construction: ArralyList 和 HashMap

---
+++ src/com/collect/ArrayListTest.java
@@ -0,0 +1,37 @@
+package com.collect;
+
+import java.util.*;
+
+/**
+ * @Auther: yangguoqiang01
+ * @Date: 2019-05-04
+ * @Description: com.collect
+ * @version: 1.0
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+
+        //List<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("您好");
+        list.add("羊羊羊");
+
+        // 第一种遍历方法使用foreach遍历List
+        for (String str : list) {  // 也可以改写for(int i=0;i<list.size();i++)这种形式
+            System.out.println(str);
+        }
+
+        // 第二种遍历，把链表变为数组相关的内容进行遍历
+        String[] strArray = new String[list.size()];
+        list.toArray(strArray);
+        for(int i = 0; i < strArray.length; i++) { //这里也可以改写为  foreach(String str:strArray)这种形式
+            System.out.println(strArray[i]);
+        }
+
+        //第三种遍历 使用迭代器进行相关遍历
+        Iterator<String> ite=list.iterator();
+        while(ite.hasNext()) { // 判断下一个元素之后有值
+            System.out.println(ite.next());
+        }
+    }
+}
