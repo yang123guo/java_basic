0fba546cfec7ed795fd1da96ede3dd0f55515354
yangguoqiang01 <wochange@163.com>
2019-05-09 08:46:15 +0800

    :construction: Collection集合

--- src/com/collect/ArrayListTest.java
+++ src/com/collect/ArrayListTest.java
@@ -33,8 +33,8 @@ public class ArrayListTest {
             System.out.println(list.get(i));
         }
 
-        //第三种遍历 使用迭代器进行相关遍历
-        Iterator<String> ite = list.iterator();
+        //第三种遍历 使用 迭代器 进行相关遍历
+        Iterator<String> ite = list.iterator();  // 获取迭代器
         while (ite.hasNext()) { // 判断下一个元素之后有值
             System.out.println(ite.next());
         }
